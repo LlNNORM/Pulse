f3f5f73e8fbe5b5f90f8da99f949f498bcb8a77a
Ivan <linnorm@ya.ru>
2023-12-26 16:45:16 +0300

    Catalog is made

--- src/js/script.js
+++ src/js/script.js
@@ -3,6 +3,7 @@
     container: '.carousel__inner',
     items: 1,
     slideBy: 'page',
+    nav: false,
     controlsText: ['<img src="icons/arrow_left.png" alt="arrow"/>',
                     '<img src="icons/arrow_right.png" alt="arrow"/>']
   });
