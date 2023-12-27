dcc3f5a7010553810313d44c82ace3a8b028fab8
Ivan <linnorm@ya.ru>
2023-12-27 15:48:19 +0300

    Tabs are activated

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,31 @@
+const slider = tns({
+  container: '.carousel__inner',
+  items: 1,
+  slideBy: 'page',
+  nav: false,
+  controlsText: ['<img src="icons/arrow_left.png" alt="arrow"/>',
+                  '<img src="icons/arrow_right.png" alt="arrow"/>']
+});
 
-  const slider = tns({
-    container: '.carousel__inner',
-    items: 1,
-    slideBy: 'page',
-    nav: false,
-    controlsText: ['<img src="icons/arrow_left.png" alt="arrow"/>',
-                    '<img src="icons/arrow_right.png" alt="arrow"/>']
-  });
+  (function($) {
+    $(function() {
+      
+      $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+      });
+
+      function  toggleSlide(item) {
+        $(item).each (function(i) {
+          $(this).on('click', function(e){
+            e.preventDefault();
+            $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+            $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+          })
+        })
+      }
+      toggleSlide('.catalog-item__link');
+      toggleSlide('.catalog-item__back');
+    });
+    })(jQuery);
